11e55e15634bb8a5922cb53f08f684f5a3495eca
Elena <elena_sadler@yahoo.com>
2021-01-28 18:36:31 -0600

    Initial commit

--- TicTacToes/TicTacToesApp.swift
+++ TicTacToes/TicTacToesApp.swift
@@ -4,15 +4,14 @@
 //
 //  Created by Elena Sadler on 1/20/21.
 //
-
 import SwiftUI
 
 @main
 struct TicTacToesApp: App {
-    
+    @StateObject private var viewModel = BoardViewModel(board: GameBoard())
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(viewModel)
         }
     }
 }
